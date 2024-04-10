0c2a29c9e2fe29600c3974a834b2eb847b80710c
Drew Kuyoth <drewanthonykuyoth@gmail.com>
2024-04-10 17:53:19 -0600

    Finished models and added some more HTML

--- FinalProject/athletic_department/models.py
+++ FinalProject/athletic_department/models.py
@@ -3,35 +3,69 @@ from django.db import models
 class Team(models.Model):
     name = models.CharField(max_length=100)
     sport_type = models.CharField(max_length=100)
-    # Add other fields as necessary
+    email = models.EmailField()
+    established_date = models.DateField()
 
 class Employee(models.Model):
-    name = models.CharField(max_length=100)
-    salary = models.DecimalField(max_digits=10, decimal_places=2)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    # Add other fields as necessary
+    employeeid = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    firstname = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    TYPE_CHOICES = (
+        ('Salary', 'Salary'),
+        ('Hourly', 'Hourly'),
+    )
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
 
 class Athlete(models.Model):
-    name = models.CharField(max_length=100)
-    scholarship_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    # Add other fields as necessary
+    athleteid = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    firstname = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    academic_level = models.CharField(max_length=100)
+    contact = models.CharField(max_length=100)
 
 class Equipment(models.Model):
-    name = models.CharField(max_length=100)
-    cost = models.DecimalField(max_digits=10, decimal_places=2)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    # Add other fields as necessarycd
+    equipmentid = models.CharField(max_length=100)
+    teamid = models.ForeignKey(Team, on_delete=models.CASCADE)
+    annual_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    year = models.PositiveSmallIntegerField()
 
 class Event(models.Model):
-    name = models.CharField(max_length=100)
+    eventid = models.CharField(max_length=100)
+    teamid = models.ForeignKey(Team, on_delete=models.CASCADE)
+    venue = models.CharField(max_length=100)
     date = models.DateField()
-    income = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    expense = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    # Add other fields as necessary
+    income = models.DecimalField(max_digits=10, decimal_places=2)
+    expenses = models.DecimalField(max_digits=10, decimal_places=2)
+    opponent = models.CharField(max_length=100)
+
+class Rank(models.Model):
+    rankid = models.CharField(max_length=100)
+    teamid = models.ForeignKey(Team, on_delete=models.CASCADE)
+    rank = models.IntegerField()
+    date = models.DateField()
+
+class Scholarship(models.Model):
+    scholarshipid = models.CharField(max_length=100)
+    athleteid = models.ForeignKey(Athlete, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    donor = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+
+class Income(models.Model):
+    incomeid = models.CharField(max_length=100)
+    teamid = models.ForeignKey(Team, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    year = models.PositiveSmallIntegerField()
 
 # Remember to run migrations after defining your models
 
-#new comment this is drew
+
 
